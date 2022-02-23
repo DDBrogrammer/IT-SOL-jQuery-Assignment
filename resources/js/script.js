9aedc725936da9a162c73ce2b38e950d71ebe335
Devilcoder-666 <zexaldai@gmail.com>
2022-02-23 17:57:15 +0700

    night work

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,50 +1,104 @@
-var tempData ;
+let tempData;
+let checkbox = false;
+let chosenStudentId = null;
+let runFirst = false;
 $(document).ready(function(){
+    if(runFirst===false){
+        $.getJSON("data.json", function(data){
+            tempData=data;
+            tempData.forEach(
+                element=>{
+                    $("tbody").append(  '<tr>'+
+                        '<td><input type="checkbox" id=" '+ element.id +  ' " name="edit" ></td>'+
+                        '<td>'+element.id+ '</td>'+
+                        '<td>'+element.name+ '</td>'+
+                        '<td>'+element.birthday + '</td>'+
+                        '<td>'+element.phone + '</td>'+
+                        '<td>'+ element.hometown + '</td>'+
+                        '</tr>')
+                }
+            );
+        })
+            .fail(function(){
+                console.log("An error has occurred.");
+            });
+        runFirst=true;
+    }
+
+
+    const countChecked = () => {
+        let count = 0;
+        for (const checkbox of document.querySelectorAll('input[type="checkbox"]')) {
+            if (checkbox.checked === true) {
+                count++;
+            }
+        }
+        return count;
+    }
+    $('table').change(
+        function () {
+            let count = countChecked();
+            console.log(count)
+            checkbox=true;
+            if (count === 0) {
+                $("#edit").prop('disabled', true);
+                $("#delete").prop('disabled', true);
+            } else {
+                $("#edit").prop('disabled', false);
+                $("#delete").prop('disabled', false);
+            }
+        });
+
     document.querySelector('form').addEventListener('submit', (e) => {
         const data = Object.fromEntries(new FormData(e.target).entries());
         data.id =tempData.length +1;
         tempData.push(data);
-                $("tbody").append(  '<tr>'+
-                    '<td><input type="checkbox" id=" '+ data.id +  ' " name="edit" ></td>'+
-                    '<td>'+data.id+ '</td>'+
-                    '<td>'+data.name+ '</td>'+
-                    '<td>'+data.birthday + '</td>'+
-                    '<td>'+data.phone + '</td>'+
-                    '<td>'+ data.hometown + '</td>'+
-                    '</tr>')
+        if(checkbox===false){
+            $("tbody").append(  '<tr>'+
+                '<td><input type="checkbox" id= "'+ data.id +  '" name="edit" ></td>'+
+                '<td>'+data.id+ '</td>'+
+                '<td>'+data.name+ '</td>'+
+                '<td>'+data.birthday + '</td>'+
+                '<td>'+data.phone + '</td>'+
+                '<td>'+ data.hometown + '</td>'+
+                '</tr>')
+        }else{
+            var row = $("input[type=checkbox]:checked").closest("tr")[0];
+            row.cells[1].innerHTML = data.name;
+            row.cells[2].innerHTML = data.birthday;
+            row.cells[3].innerHTML = data.phone;
+            row.cells[4].innerHTML = data.hometown;
+            $("input[type=checkbox]:checked").prop("checked", false);
+            alert("đã sửa sinh viên")
+            var student = { "name": $("#name").val(), "birthday": $("#birthday").val(), "phone": $("#phone").val(), "hometown": $("#hometown").val() }
+            data[currentId] = student;
+            console.log(currentId);
+            console.log(data.toString);
+            currentId = null;
+            resetStudentForm();
+        }
+
     });
-    $.getJSON("data.json", function(data){
-       tempData=data;
-       tempData.forEach(
-           element=>{
-               $("tbody").append(  '<tr>'+
-                   '<td><input type="checkbox" id=" '+ element.id +  ' " name="edit" ></td>'+
-                '<td>'+element.id+ '</td>'+
-                   '<td>'+element.name+ '</td>'+
-                   '<td>'+element.birthday + '</td>'+
-                   '<td>'+element.phone + '</td>'+
-                '<td>'+ element.hometown + '</td>'+
-              '</tr>')
-           }
-       );
-       })
-        .fail(function(){
-            console.log("An error has occurred.");
-        });
+
+
+
+    $("#edit").click(function () {
+        let count = countChecked();
+        if (count > 1) {
+            alert("chỉ được chọn 1 học sinh ")
+        } else {
+            $("Table input[type=checkbox]:checked").each(function () {
+                var row = $(this).closest("tr")[0];
+                chosenStudentId = $(this).attr("id");
+                $("#name").val(row.cells[1].innerHTML);
+                $("#birthday").val(row.cells[2].innerHTML);
+                $("#mobile-phone").val(row.cells[3].innerHTML);
+                $("#hometown").val(row.cells[4].innerHTML);
+            });
+        }
+    });
     })
 
-
-$(function(){
-    var tempNewJson;
-    $('form[name=myForm]').submit(function(){
-        $.post($(this).attr('action'), $(this).serialize(), function(json) {
-            tempNewJson=JSON.parse(json);
-            tempNewJson.id=tempData.size+1;
-            tempData.push(tempNewJson);
-        }, 'json');
-        return false;
-    });
-});
 
 
 
@@ -71,7 +125,11 @@ function validateForm() {
 
 }
 
-function deleteStudent() {
+function deleteStudent() {}
 
-
+    function resetStudentForm(){
+        $('#f-name').val("")
+        $('#f-birthday').val("")
+        $('#f-mobile-phone').val("")
+        $('#f-hometown').val("")
 }
