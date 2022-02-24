f60baf82eae140bd8414cd85758c1b08d72275fe
Devilcoder-666 <zexaldai@gmail.com>
2022-02-24 15:30:37 +0700

    all most done

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,19 +2,22 @@ let tempData;
 let checkbox = false;
 let chosenStudentId = null;
 let runFirst = false;
+let editChecked = false;
+
+
 $(document).ready(function(){
     if(runFirst===false){
         $.getJSON("data.json", function(data){
             tempData=data;
             tempData.forEach(
                 element=>{
-                    $("tbody").append(  '<tr>'+
+                    $("tbody").append(  '<tr id="'+element.id+'">'+
                         '<td><input type="checkbox" id=" '+ element.id +  ' " name="edit" ></td>'+
-                        '<td>'+element.id+ '</td>'+
-                        '<td>'+element.name+ '</td>'+
-                        '<td>'+element.birthday + '</td>'+
-                        '<td>'+element.phone + '</td>'+
-                        '<td>'+ element.hometown + '</td>'+
+                        '<td class="id">'+element.id+ '</td>'+
+                        '<td class="name" >'+element.name+ '</tdcla>'+
+                        '<td class="birthday">'+element.birthday + '</td>'+
+                        '<td class="phone">'+element.phone + '</td>'+
+                        '<td class=hometown>'+ element.hometown + '</td>'+
                         '</tr>')
                 }
             );
@@ -24,65 +27,73 @@ $(document).ready(function(){
             });
         runFirst=true;
     }
-
-
-    const countChecked = () => {
-        let count = 0;
-        for (const checkbox of document.querySelectorAll('input[type="checkbox"]')) {
-            if (checkbox.checked === true) {
-                count++;
-            }
-        }
-        return count;
-    }
     $('table').change(
         function () {
             let count = countChecked();
-            console.log(count)
+            console.log(count);
             checkbox=true;
-            if (count === 0) {
+            if (count === 0 || count>=2) {
                 $("#edit").prop('disabled', true);
                 $("#delete").prop('disabled', true);
             } else {
+                console.log("run count")
                 $("#edit").prop('disabled', false);
                 $("#delete").prop('disabled', false);
             }
         });
 
-    document.querySelector('form').addEventListener('submit', (e) => {
-        const data = Object.fromEntries(new FormData(e.target).entries());
-        data.id =tempData.length +1;
-        tempData.push(data);
-        if(checkbox===false){
-            $("tbody").append(  '<tr>'+
-                '<td><input type="checkbox" id= "'+ data.id +  '" name="edit" ></td>'+
-                '<td>'+data.id+ '</td>'+
-                '<td>'+data.name+ '</td>'+
-                '<td>'+data.birthday + '</td>'+
-                '<td>'+data.phone + '</td>'+
-                '<td>'+ data.hometown + '</td>'+
-                '</tr>')
-        }else{
-            var row = $("input[type=checkbox]:checked").closest("tr")[0];
-            row.cells[1].innerHTML = data.name;
-            row.cells[2].innerHTML = data.birthday;
-            row.cells[3].innerHTML = data.phone;
-            row.cells[4].innerHTML = data.hometown;
-            $("input[type=checkbox]:checked").prop("checked", false);
+    $("#save").click(function () {
+        let id = tempData.length + 1;
+        let name = $("#f-name").val();
+        let birthday = $("#f-birthday").val();
+        let mobile = $("#f-mobile-phone").val();
+        let hometown = $("#f-hometown").val();
+        let student = {id, name, birthday, mobile, hometown};
+        if(editChecked){
+            var baseRow=$("input[type=checkbox]:checked");
+            var row = baseRow.closest("tr")[0];
+            row.cells[2].innerHTML = $("#f-name").val();
+            row.cells[3].innerHTML = $("#f-birthday").val();
+            row.cells[4].innerHTML = $("#f-mobile-phone").val();
+            row.cells[5].innerHTML = $("#f-hometown").val();
+            baseRow.prop("checked", false);
             alert("đã sửa sinh viên")
-            var student = { "name": $("#name").val(), "birthday": $("#birthday").val(), "phone": $("#phone").val(), "hometown": $("#hometown").val() }
-            data[currentId] = student;
-            console.log(currentId);
-            console.log(data.toString);
-            currentId = null;
+            tempData[chosenStudentId] = student;
+            chosenStudentId = null;
             resetStudentForm();
-        }
+            editChecked=false;
 
+        }else {
+            $("tbody").append(  '<tr id="'+ id +'">'+
+                '<td><input type="checkbox" id=" '+ id+  ' " name="edit" ></td>'+
+                '<td>'+ id  + '</td>'+
+                '<td>'+ name+ '</td>'+
+                '<td>'+ birthday + '</td>'+
+                '<td>'+ mobile + '</td>'+
+                '<td>'+ hometown + '</td>'+
+                '</tr>')
+            tempData.push(student);
+        }
+    })
+    $("#edit").click(function () {
+        let count = countChecked();
+        if (count > 1) {
+            alert("chỉ được chọn 1 học sinh ")
+
+        } else {
+            $("Table input[type=checkbox]:checked").each(function () {
+                editChecked=true;
+                var row = $(this).closest("tr")[0];
+                chosenStudentId = $(this).attr("id");
+                $("#f-name").val(row.cells[2].innerHTML);
+                $("#f-birthday").val(row.cells[3].innerHTML);
+                $("#f-mobile-phone").val(row.cells[4].innerHTML);
+                $("#f-hometown").val(row.cells[5].innerHTML);
+            });
+        }
     });
 
-
-
-    $("#edit").click(function () {
+    $("#delete").click( function () {
         let count = countChecked();
         if (count > 1) {
             alert("chỉ được chọn 1 học sinh ")
@@ -90,43 +101,52 @@ $(document).ready(function(){
             $("Table input[type=checkbox]:checked").each(function () {
                 var row = $(this).closest("tr")[0];
                 chosenStudentId = $(this).attr("id");
-                $("#name").val(row.cells[1].innerHTML);
-                $("#birthday").val(row.cells[2].innerHTML);
-                $("#mobile-phone").val(row.cells[3].innerHTML);
-                $("#hometown").val(row.cells[4].innerHTML);
+                row.remove();
+                tempData.splice(chosenStudentId,1);
+                tempData.forEach(
+                    element=>{
+                    console.log(element.name);
+                    }
+                );
             });
         }
     });
     })
 
 
-
-
 function validateForm() {
-    var name = document.forms["myForm"]["name"].value;
-    var phone = document.forms["myForm"]["phone"].value;
-    var birthday = document.forms["myForm"]["birthday"].value;
-    var hometown = document.forms["myForm"]["hometown"].value;
-    if (name == "" || name == null) {
+    let name = document.forms["myForm"]["name"].value;
+    let phone = document.forms["myForm"]["phone"].value;
+    let birthday = document.forms["myForm"]["birthday"].value;
+    let hometown = document.forms["myForm"]["hometown"].value;
+    if (name === "" || name == null) {
         alert("Name must be filled out");
         return false;
-    }else if (phone == "" || phone == null ){
+    }else if (phone === "" || phone == null ){
         alert("Phone must be filled out");
         return  false;
     }
-    else if (birthday ==  "" || birthday == null ){
+    else if (birthday ===  "" || birthday == null ){
         alert("Birthday must be filled out");
         return  false;
     }
-    else if (hometown ==  "" || hometown == null ){
+    else if (hometown ===  "" || hometown == null ){
         alert("Hometown must be filled out");
         return  false;
     }
 
 }
 
-function deleteStudent() {}
-
+const countChecked = () => {
+    let count = 0;
+    for (const checkbox of document.querySelectorAll('input[type="checkbox"]')) {
+        if (checkbox.checked === true) {
+            count++;
+        }
+    }
+    return count;
+}
+/* ok */
     function resetStudentForm(){
         $('#f-name').val("")
         $('#f-birthday').val("")
